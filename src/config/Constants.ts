6d065f5fc368592700a24a4f690d1dba05e04b2f
Mauricio Teixeira <maufteixeira@gmail.com>
2021-10-05 20:12:40 -0300

    url shortener project

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://maufteixeira:gdc3dtqw9g@cluster0.afqeh.mongodb.net/shortener?retryWrites=true&w=majority',
+}
